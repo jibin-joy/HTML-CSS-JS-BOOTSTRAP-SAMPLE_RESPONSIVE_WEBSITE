e60c902983c560768f2f36ed397d5eb5667b505f
Jibin-Sample <jibinkjoy3101@gmail.com>
2023-12-01 13:59:32 +0530

    FrontEnd Responsive WrebSite-HTML,CSS,JS and Bootstrap

---
+++ script.js
@@ -0,0 +1,22 @@
+// Scroll back to top button
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// Hamburger menu
+function myFunction() {
+  var navbar = document.getElementsByClassName("nav");
+  navbar.classList.toggle("show");
+}
